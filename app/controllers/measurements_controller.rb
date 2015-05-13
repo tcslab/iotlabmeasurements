f5d6c0c677c70e54927da1e4d605de9c52c2dee6
aivic-unigedesk <ali.ivic88@gmail.com>
2015-05-13 13:05:03 +0200

    Implements the index action. Returns the list of all measurements.

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,2 +1,6 @@
 class MeasurementsController < ApplicationController
+  def index
+    @measurements = Measurement.all.as_json
+    render json: @measurements
+  end
 end
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,5 +1,6 @@
 class Measurement
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :experiment_id, type: Integer
   field :resource_id, type: Integer
   field :measurement_value, type: String
